4a8b08eff311a17fc832f3813f5da2dd575eecb2
millerronen <32002177+millerronen@users.noreply.github.com>
2023-05-03 10:11:33 +0530

    Add files via upload

---
+++ src/submission.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python
+
+import random
+from typing import Callable, Dict, List, Tuple, TypeVar, DefaultDict
+from util import *
+
+FeatureVector = Dict[str, int]
+WeightVector = Dict[str, float]
+Example = Tuple[FeatureVector, int]
+
+
+############################################################
+# Problem 1: binary classification
+############################################################
+
+############################################################
+# Problem 1a: feature extraction
+
+
+def extractWordFeatures(x: str) -> FeatureVector:
+    """
+    Extract word features for a string x. Words are delimited by
+    whitespace characters only.
+    @param string x:
+    @return dict: feature vector representation of x.
+    Example: "I am what I am" --> {'I': 2, 'am': 2, 'what': 1}
+    """
+    pass
+    # ### START CODE HERE ###
+    my_dict = {}
+    words = x.split()
+    for word in words:
+        if word in my_dict:
+            my_dict[word] += 1
+        else:
+            my_dict[word] = 1
+    return my_dict
+    # ### END CODE HERE ###
+
+
+############################################################
+# Problem 1b: stochastic gradient descent
+
+T = TypeVar("T")
+
+
+def learnPredictor(
+        trainExamples: List[Tuple[T, int]],
+        validationExamples: List[Tuple[T, int]],
+        featureExtractor: Callable[[T], FeatureVector],
+        numEpochs: int,
+        eta: float,
+) -> WeightVector:
+    """
+    Given |trainExamples| and |validationExamples| (each one is a list of (x,y)
+    pairs), a |featureExtractor| to apply to x, and the number of epochs to
+    train |numEpochs|, the step size |eta|, return the weight vector (sparse
+    feature vector) learned.
+
+    You should implement stochastic gradient descent.
+
+    Notes:
+    - Only use the trainExamples for training!
+    - You should call evaluatePredictor() on both trainExamples and validationExamples
+    to see how you're doing as you learn after each epoch.
+    - The predictor should output +1 if the score is precisely 0.
+    """
+    weights = {}  # feature => weight
+
+    # ### START CODE HERE ###
+    for t in range(numEpochs):
+        for i in range(len(trainExamples)):
+            x = trainExamples[i][0]
+            y = trainExamples[i][1]
+            phi_x = featureExtractor(x)
+            value = 1 - (dotProduct(weights, phi_x) * y)
+            gradient = {}
+            if value <= 0:
+                continue
+            for phi_x_feature, phi_x_value in phi_x.items():
+                gradient[phi_x_feature] = -phi_x_value * y
+            increment(weights, (-1) * eta, gradient)
+        train_error = evaluatePredictor(trainExamples,
+                                        lambda sample: (
+                                            1 if dotProduct(featureExtractor(sample), weights) >= 0 else -1))
+        dev_error = evaluatePredictor(validationExamples,
+                                      lambda sample: (1 if dotProduct(featureExtractor(sample), weights) >= 0 else -1))
+        if train_error < 0.04 and dev_error < 0.30:
+            break
+    return weights
+    # ### END CODE HERE ###
+
+
+############################################################
+# Problem 1c: generate test case
+
+
+def generateDataset(numExamples: int, weights: WeightVector) -> List[Example]:
+    """
+    Return a set of examples (phi(x), y) randomly which are classified correctly by
+    |weights|.
+    """
+    random.seed(42)
+
+    # Return a single example (phi(x), y).
+    # phi(x) should be a dict whose keys are a subset of the keys in weights
+    # and values can be anything (randomize!) with a score for the given weight vector.
+    # y should be 1 or -1 as classified by the weight vector.
+    # y should be 1 if the score is precisely 0.
+
+    # Note that the weight vector can be arbitrary during testing.
+    def generateExample() -> Tuple[Dict[str, int], int]:
+        phi = {}
+        # ### START CODE HERE ###
+        random_key = random.choice(list(weights.keys()))
+        phi[random_key] = weights[random_key]
+        y = 1 if dotProduct(weights, phi) >= 0 else -1
+        # ### END CODE HERE ###
+        return phi, y
+
+    return [generateExample() for _ in range(numExamples)]
+
+
+############################################################
+# Problem 1d: character features
+
+
+def extractCharacterFeatures(n: int) -> Callable[[str], FeatureVector]:
+    """
+    Return a function that takes a string |x| and returns a sparse feature
+    vector consisting of all n-grams of |x| without spaces mapped to their n-gram counts.
+    EXAMPLE: (n = 3) "I like tacos" --> {'Ili': 1, 'lik': 1, 'ike': 1, ...
+    You may assume that n >= 1.
+    """
+
+    def extract(x):
+        pass
+        # ### START CODE HERE ###
+        # ### END CODE HERE ###
+
+    return extract
+
+
+############################################################
+# Problem 1e:
+#
+# Helper function to test 1e.
+#
+# To run this function, run the command from termial with `n` replaced
+#
+# $ python -c "from submission import *; testValuesOfN(n)"
+#
+
+
+def testValuesOfN(n: int):
+    """
+    Use this code to test different values of n for extractCharacterFeatures
+    This code is exclusively for testing.
+    Your full written solution for this problem must be submitted.
+    """
+    trainExamples = readExamples("polarity.train")
+    validationExamples = readExamples("polarity.dev")
+    featureExtractor = extractCharacterFeatures(n)
+    weights = learnPredictor(
+        trainExamples, validationExamples, featureExtractor, numEpochs=20, eta=0.01
+    )
+    outputWeights(weights, "weights")
+    outputErrorAnalysis(
+        validationExamples, featureExtractor, weights, "error-analysis"
+    )  # Use this to debug
+    trainError = evaluatePredictor(
+        trainExamples,
+        lambda x: (1 if dotProduct(featureExtractor(x), weights) >= 0 else -1),
+    )
+    validationError = evaluatePredictor(
+        validationExamples,
+        lambda x: (1 if dotProduct(featureExtractor(x), weights) >= 0 else -1),
+    )
+    print(
+        (
+                "Official: train error = %s, validation error = %s"
+                % (trainError, validationError)
+        )
+    )
+
+
+############################################################
+# Problem 2b: K-means
+############################################################
+
+
+def kmeans(
+        examples: List[Dict[str, float]], K: int, maxEpochs: int
+) -> Tuple[List, List, float]:
+    """
+    examples: list of examples, each example is a string-to-float dict representing a sparse vector.
+    K: number of desired clusters. Assume that 0 < K <= |examples|.
+    maxEpochs: maximum number of epochs to run (you should terminate early if the algorithm converges).
+    Return: (length K list of cluster centroids,
+            list of assignments (i.e. if examples[i] belongs to centers[j], then assignments[i] = j),
+            final reconstruction loss)
+    """
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
